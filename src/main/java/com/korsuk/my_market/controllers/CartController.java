aff2af4911e36627e87e5ce1763467652e3c2304
Kirill Korsuk <korsukkirill@gmail.com>
2022-11-11 19:48:58 +0300

    homework cart rest api

--- src/main/java/com/korsuk/my_market/controllers/CartController.java
+++ src/main/java/com/korsuk/my_market/controllers/CartController.java
@@ -4,13 +4,16 @@ import com.korsuk.my_market.dto.NovelDto;
 import com.korsuk.my_market.services.CartService;
 import com.korsuk.my_market.services.NovelService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/cart")
+@Slf4j
 public class CartController {
 
     private final CartService cartService;
@@ -22,10 +25,17 @@ public class CartController {
         return cartService.novelsInCart();
     }
 
+    @GetMapping("/dto")
+    public Set<NovelDto> getNovelsNotEntity() {
+        return cartService.novelsInCartNotEntity();
+    }
+
     @DeleteMapping("/delete_novel")
     public void deleteBook(@RequestParam Long novel_id){
 //        novelService.getNovelById(novel_id);
-        cartService.deleteFromCart(novel_id);
+        NovelDto novelDto = new NovelDto(novelService.getNovelById(novel_id));
+        log.info(novelDto.toString() + "from cartController");
+        cartService.deleteFromCart(novelDto);
     }
 
 
--- src/main/java/com/korsuk/my_market/services/CartService.java
+++ src/main/java/com/korsuk/my_market/services/CartService.java
@@ -1,6 +1,5 @@
 package com.korsuk.my_market.services;
 
-import com.korsuk.my_market.dto.AuthorDto;
 import com.korsuk.my_market.dto.NovelDto;
 import com.korsuk.my_market.products.Cart;
 import com.korsuk.my_market.products.Novel;
@@ -9,16 +8,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class CartService {
 
     private final CartRepository cartRepository;
+    private final CartNotEntity cartNotEntity;
 
     public List<NovelDto> novelsInCart() {
       List<Cart> carts = cartRepository.findAll();
@@ -27,9 +27,14 @@ public class CartService {
       return novelsDto;
     }
 
-    @Transactional
-    public void deleteFromCart(Long id) {
-        cartRepository.deleteCartByNovel_Id(id);
+    public Set<NovelDto> novelsInCartNotEntity() {
+        return cartNotEntity.cartNovels(cartNotEntity.getNovelsInCart());
+    }
+
+//    @Transactional
+    public void deleteFromCart(NovelDto novelDto) {
+//        cartRepository.deleteCartByNovel_Id(id);
+        cartNotEntity.deleteFromCart(novelDto);
     }
 
     public Cart save(Cart cart) {
